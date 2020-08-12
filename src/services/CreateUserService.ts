ee4c3a599464069cc9791329c57fb1c2c40c5264
Jorge Falca <jfalca@live.com>
2020-08-11 22:39:45 -0300

    Criptografia de senha

--- src/services/CreateUserService.ts
+++ src/services/CreateUserService.ts
@@ -1,4 +1,5 @@
 import { getRepository } from 'typeorm';
+import { hash } from 'bcryptjs';
 import User from '../models/User';
 
 interface Request {
@@ -19,10 +20,11 @@ class CreateUserService {
       throw new Error('Email address already used.');
     }
 
+    const hashPassword = await hash(password, 8);
     const user = userRepository.create({
       name,
       email,
-      password,
+      password: hashPassword,
     });
 
     await userRepository.save(user);
